c185ed8c8ccc27101438528bad5ad212681d92cc
firman-maulana <firmanjabbar6@gmail.com>
2026-01-23 16:42:19 +0700

    solve db column mode

---
+++ database/migrations/2026_01_23_094032_alter_mode_column_in_programs_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('programs', function (Blueprint $table) {
+            $table->enum('mode', [
+                'Online & Offline',
+                'Online',
+                'Offline'
+            ])->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('programs', function (Blueprint $table) {
+            $table->enum('mode', ['Online', 'Offline'])->change();
+        });
+    }
+};
